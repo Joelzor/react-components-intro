37e1db0b8d0444e4c549966ea800aa889ec5027b
Joel Watkins <eck087@yahoo.co.uk>
2022-11-03 12:44:22 +0000

    main done

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import LeftNav from "./Left-nav";
+import RightNav from "./Right-nav";
+
+function Header() {
+  return (
+    <header>
+      <LeftNav />
+      <RightNav />
+    </header>
+  );
+}
+
+export default Header;
